c9b0abfeb5bdb298fbbf2d6a1848e9f5f6d674d0
Emmanuel FONTAN <emmanuel.fontan@viareport.com>
2013-06-25 14:10:46 +0200

    Initial commit

---
+++ src/http/ServerExample.java
@@ -0,0 +1,22 @@
+package http;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.deploy.Verticle;
+
+public class ServerExample extends Verticle {
+
+    public void start() {
+        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest req) {
+                System.out.println("Got request: " + req.uri);
+                System.out.println("Headers are: ");
+                for (String key : req.headers().keySet()) {
+                    System.out.println(key + ":" + req.headers().get(key));
+                }
+                req.response.headers().put("Content-Type", "text/html; charset=UTF-8");
+                req.response.end("<html><body><h1>Hello from vert.x!</h1></body></html>");
+            }
+        }).listen(8080);
+    }
+}
