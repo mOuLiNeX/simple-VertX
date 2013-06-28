7372d4629a3e9edffb17dff7b5de4d0faa8abc61
Emmanuel FONTAN <emmanuel.fontan@viareport.com>
2013-06-28 09:49:02 +0200

    Ajout event bus

--- src/http/ServerExample.java
+++ src/http/ServerExample.java
@@ -1,13 +1,16 @@
 package http;
 
 import org.vertx.java.core.Handler;
+import org.vertx.java.core.http.HttpServer;
 import org.vertx.java.core.http.HttpServerRequest;
 import org.vertx.java.deploy.Verticle;
 
 public class ServerExample extends Verticle {
 
     public void start() {
-        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+        HttpServer server = vertx.createHttpServer();
+
+        server.requestHandler(new Handler<HttpServerRequest>() {
             public void handle(HttpServerRequest req) {
                 System.out.println("Got request: " + req.uri);
                 System.out.println("Headers are: ");
